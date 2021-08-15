9e4f1d6ceae0eb387f4d906797c5132ab10116ff
Juan Torchia <j.s.torchia@gmail.com>
2021-08-15 02:56:46 -0300

    Funcionando

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "../assets/styles/Main.scss";
 import 'bootstrap';
 import NavBar from './NavBar';
